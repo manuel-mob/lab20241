b74641986ef40bf216d276542fd4a792849c965c
Manuel Alejandro Moscoso Domínguez <manuel@mobiquos.cl>
2024-07-05 07:50:10 -0400

    First version of splash screen with flutter_native_splash: ^2.4.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
 import 'package:ejemplo_lab/pages/detail.dart';
 import 'package:flutter/material.dart';
 import 'pages/homepage.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 Future<void> main() async {
-  
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
-  WidgetsFlutterBinding.ensureInitialized();
+  //WidgetsFlutterBinding.ensureInitialized();
    
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
+
+  const MyApp({super.key});
+  
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -4,6 +4,7 @@ import 'package:ejemplo_lab/pages/list_games.dart';
 import 'package:ejemplo_lab/pages/practice_future_example.dart';
 import 'package:ejemplo_lab/pages/sensors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'detail.dart';
 import 'list_detail.dart';
 import 'list_players.dart';
@@ -31,9 +32,26 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
+    initialization();
     print("initState() called.");
     _playersFuture = Player.loadPlayers();
     _players = _buildPlayersList();
+  }
+  
+
+  void initialization() async {
+    // This is where you can initialize the resources needed by your app while
+    // the splash screen is displayed.  Remove the following example because
+    // delaying the user experience is a bad design practice!
+    // ignore_for_file: avoid_print
+    print('ready in 3...');
+    await Future.delayed(const Duration(seconds: 1));
+    print('ready in 2...');
+    await Future.delayed(const Duration(seconds: 1));
+    print('ready in 1...');
+    await Future.delayed(const Duration(seconds: 1));
+    print('go!');
+    FlutterNativeSplash.remove();
   }
   @override
   void didChangeDependencies() {
